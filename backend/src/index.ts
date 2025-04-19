7bc96d678b2871f2fbc31e7604c9d7215c057d29
Jason Ly <81354745+jasonly027@users.noreply.github.com>
2025-04-18 17:24:46 -0700

    Init backend

---
+++ backend/src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const PORT = 8080;
+
+app.get("/", (req, res) => {
+    res.send("HelloWorld");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:8080`);
+})
